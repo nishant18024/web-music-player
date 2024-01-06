3a8a6148b1f164e255d9dec47b5b473722a782e8
nishant18024 <67552233+nishant18024@users.noreply.github.com>
2024-01-07 00:20:57 +0530

    views update

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const path = require('path')
 const app = express(); File
 const PORT = 8080;
 
+// view Engine --> EJS
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// Static files --> CSS and Script Files
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
     console.log(`Server is running at Port: ${PORT}`);
